a67b7e8f9142f91ac17aeb62d05e2f7d4f6e8efd
Süleyman ÇETİN <suleyman.cetin.fc@outlook.com>
2023-06-04 18:56:33 +0300

    working but should be improved

--- src/MultiplicationTableApp.java
+++ src/MultiplicationTableApp.java
@@ -6,287 +6,252 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MultiplicationTableApp {
+    private static final int DEFAULT_QUESTION_COUNT = 10;
+    private static final int DEFAULT_MAX_OPERAND = 8;
+    private static final int DEFAULT_TIME_LIMIT = 60;
+    private static final int MAX_SCORE = 60;
+
     private JFrame frame;
     private JPanel loginPanel;
     private JPanel parentPanel;
     private JPanel childPanel;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
     private JButton loginButton;
-    private Map<String, String> parentCredentials;
-    private Map<String, Child> children;
-
-    private int operandA;
-    private int operandB;
+    private JButton logoutButton;
+    private JButton startButton;
+    private JTextField parentUsernameField;
+    private JPasswordField parentPasswordField;
+    private Map<String, String> children;
+    private String loggedInUser;
     private int questionCount;
+    private int maxOperand;
+    private int timeLimit;
+    private int bestScore;
 
     public MultiplicationTableApp() {
-        parentCredentials = new HashMap<>();
-        parentCredentials.put("parent", "admin");
-
-        children = new HashMap<>();
-        children.put("child1", new Child("child1", "pass1"));
-        children.put("child2", new Child("child2", "pass2"));
-
-        operandA = 5;
-        operandB = 8;
-        questionCount = 10;
-
-        initializeGUI();
-        setupLoginPanel();
-        setupParentPanel();
-        setupChildPanel();
-    }
-
-    private void initializeGUI() {
         frame = new JFrame("Multiplication Table App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLayout(new CardLayout());
 
-        loginPanel = new JPanel();
-        parentPanel = new JPanel();
-        childPanel = new JPanel();
+        children = new HashMap<>();
 
-        frame.add(loginPanel, "login");
-        frame.add(parentPanel, "parent");
-        frame.add(childPanel, "child");
+        createLoginPanel();
+        createParentPanel();
+        createChildPanel();
+
+        showLoginPanel();
+
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setVisible(true);
     }
 
-    private void setupLoginPanel() {
-        loginPanel.setLayout(new FlowLayout());
+    private void createLoginPanel() {
+        loginPanel = new JPanel(new BorderLayout());
 
+        JPanel formPanel = new JPanel(new GridLayout(3, 2));
         JLabel usernameLabel = new JLabel("Username:");
-        loginPanel.add(usernameLabel);
-
-        usernameField = new JTextField(10);
-        loginPanel.add(usernameField);
-
         JLabel passwordLabel = new JLabel("Password:");
-        loginPanel.add(passwordLabel);
-
-        passwordField = new JPasswordField(10);
-        loginPanel.add(passwordField);
+        parentUsernameField = new JTextField();
+        parentPasswordField = new JPasswordField();
+        formPanel.add(usernameLabel);
+        formPanel.add(parentUsernameField);
+        formPanel.add(passwordLabel);
+        formPanel.add(parentPasswordField);
 
         loginButton = new JButton("Login");
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String username = usernameField.getText();
-                String password = new String(passwordField.getPassword());
-
-                if (parentCredentials.containsKey(username) && parentCredentials.get(username).equals(password)) {
-                    usernameField.setText("");
-                    passwordField.setText("");
-                    frame.getContentPane().removeAll();
-                    frame.getContentPane().add(parentPanel);
-                    frame.getContentPane().revalidate();
-                    frame.getContentPane().repaint();
-                } else if (children.containsKey(username) && children.get(username).getPassword().equals(password)) {
-                    usernameField.setText("");
-                    passwordField.setText("");
-                    frame.getContentPane().removeAll();
-                    frame.getContentPane().add(childPanel);
-                    frame.getContentPane().revalidate();
-                    frame.getContentPane().repaint();
-                    startQuiz(username);
+                String username = parentUsernameField.getText();
+                String password = new String(parentPasswordField.getPassword());
+                if (username.equals("parent") && password.equals("admin")) {
+                    loggedInUser = username;
+                    showParentPanel();
+                } else if (children.containsKey(username) && children.get(username).equals(password)) {
+                    loggedInUser = username;
+                    showChildPanel();
                 } else {
-                    JOptionPane.showMessageDialog(frame, "Invalid credentials", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(frame, "Invalid credentials", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
-        loginPanel.add(loginButton);
+
+        loginPanel.add(formPanel, BorderLayout.CENTER);
+        loginPanel.add(loginButton, BorderLayout.SOUTH);
     }
 
-    private void setupParentPanel() {
-        parentPanel.setLayout(new FlowLayout());
+    private void createParentPanel() {
+        parentPanel = new JPanel(new BorderLayout());
 
-        JLabel aLabel = new JLabel("Operand A:");
-        JTextField aField = new JTextField(String.valueOf(operandA), 10);
-        parentPanel.add(aLabel);
-        parentPanel.add(aField);
+        JPanel settingsPanel = new JPanel(new GridLayout(4, 2));
+        JLabel questionCountLabel = new JLabel("Question Count:");
+        JLabel maxOperandLabel = new JLabel("Max Operand:");
+        JLabel timeLimitLabel = new JLabel("Time Limit (seconds):");
+        JTextField questionCountField = new JTextField(Integer.toString(DEFAULT_QUESTION_COUNT));
+        JTextField maxOperandField = new JTextField(Integer.toString(DEFAULT_MAX_OPERAND));
+        JTextField timeLimitField = new JTextField(Integer.toString(DEFAULT_TIME_LIMIT));
+        settingsPanel.add(questionCountLabel);
+        settingsPanel.add(questionCountField);
+        settingsPanel.add(maxOperandLabel);
+        settingsPanel.add(maxOperandField);
+        settingsPanel.add(timeLimitLabel);
+        settingsPanel.add(timeLimitField);
 
-        JLabel bLabel = new JLabel("Operand B:");
-        JTextField bField = new JTextField(String.valueOf(operandB), 10);
-        parentPanel.add(bLabel);
-        parentPanel.add(bField);
-
-        JLabel countLabel = new JLabel("Question Count:");
-        JTextField countField = new JTextField(String.valueOf(questionCount), 10);
-        parentPanel.add(countLabel);
-        parentPanel.add(countField);
-
-        JButton updateButton = new JButton("Update");
-        updateButton.addActionListener(new ActionListener() {
+        JPanel childrenPanel = new JPanel(new GridLayout(0, 2));
+        JLabel childUsernameLabel = new JLabel("Child Username:");
+        JLabel childPasswordLabel = new JLabel("Child Password:");
+        JTextField childUsernameField = new JTextField();
+        JPasswordField childPasswordField = new JPasswordField();
+        JButton addChildButton = new JButton("Add Child");
+        addChildButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                operandA = Integer.parseInt(aField.getText());
-                operandB = Integer.parseInt(bField.getText());
-                questionCount = Integer.parseInt(countField.getText());
-                JOptionPane.showMessageDialog(frame, "Settings updated successfully", "Update Successful", JOptionPane.INFORMATION_MESSAGE);
+                String childUsername = childUsernameField.getText();
+                String childPassword = new String(childPasswordField.getPassword());
+
+                if (childUsername.isEmpty() || childPassword.isEmpty()) {
+                    JOptionPane.showMessageDialog(frame, "Please enter both username and password for the child", "Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    children.put(childUsername, childPassword);
+                    JOptionPane.showMessageDialog(frame, "Child added successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    childUsernameField.setText("");
+                    childPasswordField.setText("");
+                }
             }
         });
-        parentPanel.add(updateButton);
-    }
+        childrenPanel.add(childUsernameLabel);
+        childrenPanel.add(childUsernameField);
+        childrenPanel.add(childPasswordLabel);
+        childrenPanel.add(childPasswordField);
+        childrenPanel.add(addChildButton);
 
-    private void setupChildPanel() {
-        childPanel.setLayout(new BorderLayout());
-
-        JPanel questionPanel = new JPanel();
-        JLabel questionLabel = new JLabel("Question:");
-        JLabel questionText = new JLabel();
-        JTextField answerField = new JTextField(5);
-        JButton submitButton = new JButton("Submit");
-        JLabel counterLabel = new JLabel("Time left: 60 seconds");
-        JLabel scoreLabel = new JLabel("Score: 0");
-        JLabel resultLabel = new JLabel();
-
-        submitButton.addActionListener(new ActionListener() {
-            private int questionNumber = 1;
-            private int score = 0;
-            private long startTime;
-
+        JButton saveSettingsButton = new JButton("Save Settings");
+        saveSettingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (questionNumber > questionCount) {
-                    JOptionPane.showMessageDialog(frame, "You have answered all the questions!\nFinal score: " + score,
-                            "Quiz Completed", JOptionPane.INFORMATION_MESSAGE);
-                    children.get(usernameField.getText()).setScore(score);
-                    frame.getContentPane().removeAll();
-                    frame.getContentPane().add(loginPanel);
-                    frame.getContentPane().revalidate();
-                    frame.getContentPane().repaint();
-                    return;
-                }
-
-                String answer = answerField.getText();
-
-                if (answer.isEmpty()) {
-                    JOptionPane.showMessageDialog(frame, "Please enter an answer.", "Answer Required", JOptionPane.WARNING_MESSAGE);
-                    return;
-                }
-
-                int result = operandA * operandB;
-                long endTime = System.currentTimeMillis();
-                long elapsedTime = (endTime - startTime) / 1000;
-                int timeLeft = 60 - (int) elapsedTime;
-
-                if (Integer.parseInt(answer) == result && timeLeft >= 0) {
-                    score += 60 - timeLeft;
-                    resultLabel.setText("Correct!");
-                } else {
-                    resultLabel.setText("Wrong!");
-                }
-
-                answerField.setText("");
-                questionNumber++;
-                scoreLabel.setText("Score: " + score);
-                counterLabel.setText("Time left: " + timeLeft + " seconds");
-
-                if (questionNumber > questionCount) {
-                    submitButton.setText("Finish");
-                }
-
-                startTime = System.currentTimeMillis();
-
-                // Display the next question
-                int nextOperandA = (int) (Math.random() * operandA) + 1;
-                int nextOperandB = (int) (Math.random() * operandB) + 1;
-                questionText.setText(nextOperandA + " x " + nextOperandB);
+                questionCount = Integer.parseInt(questionCountField.getText());
+                maxOperand = Integer.parseInt(maxOperandField.getText());
+                timeLimit = Integer.parseInt(timeLimitField.getText());
+                JOptionPane.showMessageDialog(frame, "Settings saved!", "Success", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
-        questionPanel.add(questionLabel);
-        questionPanel.add(questionText);
-        questionPanel.add(answerField);
-        questionPanel.add(submitButton);
-        questionPanel.add(counterLabel);
-        questionPanel.add(scoreLabel);
-        questionPanel.add(resultLabel);
-
-        childPanel.add(questionPanel, BorderLayout.CENTER);
-
-        // Initial question display
-        int initialOperandA = (int) (Math.random() * operandA) + 1;
-        int initialOperandB = (int) (Math.random() * operandB) + 1;
-        questionText.setText(initialOperandA + " x " + initialOperandB);
+        parentPanel.add(settingsPanel, BorderLayout.CENTER);
+        parentPanel.add(childrenPanel, BorderLayout.NORTH);
+        parentPanel.add(saveSettingsButton, BorderLayout.SOUTH);
     }
 
-    private void startQuiz(String childUsername) {
-        Child child = children.get(childUsername);
-        JLabel counterLabel = (JLabel) ((JPanel) childPanel.getComponent(0)).getComponent(4);
-        Thread timerThread = new Thread(new Runnable() {
-            private int timeLeft = 60;
+    private void createChildPanel() {
+        childPanel = new JPanel(new BorderLayout());
 
+        JLabel childInfoLabel = new JLabel();
+        JLabel scoreLabel = new JLabel();
+
+        startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
             @Override
-            public void run() {
-                while (timeLeft >= 0) {
-                    try {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                counterLabel.setText("Time left: " + timeLeft + " seconds");
-                            }
-                        });
-                        Thread.sleep(1000);
-                        timeLeft--;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                JOptionPane.showMessageDialog(frame, "Time's up!", "Quiz Completed", JOptionPane.INFORMATION_MESSAGE);
-                children.get(childUsername).setScore(0);
-                frame.getContentPane().removeAll();
-                frame.getContentPane().add(loginPanel);
-                frame.getContentPane().revalidate();
-                frame.getContentPane().repaint();
+            public void actionPerformed(ActionEvent e) {
+                startQuiz(childInfoLabel, scoreLabel);
             }
         });
-        timerThread.start();
+
+        logoutButton = new JButton("Logout");
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loggedInUser = null;
+                showLoginPanel();
+            }
+        });
+
+        childPanel.add(childInfoLabel, BorderLayout.NORTH);
+        childPanel.add(startButton, BorderLayout.CENTER);
+        childPanel.add(scoreLabel, BorderLayout.SOUTH);
     }
 
-    public void showLoginScreen() {
+    private void showLoginPanel() {
         frame.getContentPane().removeAll();
         frame.getContentPane().add(loginPanel);
         frame.getContentPane().revalidate();
         frame.getContentPane().repaint();
     }
 
-    public void startApp() {
-        showLoginScreen();
-        frame.setVisible(true);
+    private void showParentPanel() {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(parentPanel);
+        frame.getContentPane().add(logoutButton, BorderLayout.SOUTH);
+        frame.getContentPane().revalidate();
+        frame.getContentPane().repaint();
+    }
+
+    private void showChildPanel() {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(childPanel);
+        frame.getContentPane().add(logoutButton, BorderLayout.SOUTH);
+        frame.getContentPane().revalidate();
+        frame.getContentPane().repaint();
+    }
+
+    private void startQuiz(JLabel childInfoLabel, JLabel scoreLabel) {
+        int currentScore = 0;
+        int questionNumber = 1;
+        long startTime = System.currentTimeMillis();
+
+        while (questionNumber <= questionCount) {
+            int operand1 = (int) (Math.random() * maxOperand) + 1;
+            int operand2 = (int) (Math.random() * maxOperand) + 1;
+            int correctAnswer = operand1 * operand2;
+
+            String userAnswer = JOptionPane.showInputDialog(
+                    frame,
+                    "Question " + questionNumber + ":\n" + operand1 + " x " + operand2 + " = ?",
+                    "Question",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+
+            if (userAnswer != null) {
+                long elapsedTime = System.currentTimeMillis() - startTime;
+                int timeScore = calculateTimeScore(elapsedTime);
+
+                if (userAnswer.equals(Integer.toString(correctAnswer))) {
+                    currentScore += timeScore;
+                }
+
+                questionNumber++;
+            } else {
+                break; // User canceled the quiz
+            }
+        }
+
+        int totalScore = calculateTotalScore(currentScore, questionCount);
+
+        if (totalScore > bestScore) {
+            bestScore = totalScore;
+        }
+
+        childInfoLabel.setText("Child: " + loggedInUser);
+        scoreLabel.setText("Score: " + totalScore + " (Best: " + bestScore + ")");
+    }
+
+    private int calculateTimeScore(long elapsedTime) {
+        if (elapsedTime <= 1000) {
+            return MAX_SCORE;
+        } else if (elapsedTime >= timeLimit * 1000) {
+            return 0;
+        } else {
+            double percentage = (double) (timeLimit * 1000 - elapsedTime) / (timeLimit * 1000);
+            return (int) (percentage * MAX_SCORE);
+        }
+    }
+
+    private int calculateTotalScore(int currentScore, int questionCount) {
+        return (int) ((double) currentScore / (MAX_SCORE * questionCount) * MAX_SCORE);
     }
 
     public static void main(String[] args) {
-        MultiplicationTableApp app = new MultiplicationTableApp();
-        app.startApp();
-    }
-
-    private class Child {
-        private String username;
-        private String password;
-        private int score;
-
-        public Child(String username, String password) {
-            this.username = username;
-            this.password = password;
-            this.score = 0;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public int getScore() {
-            return score;
-        }
-
-        public void setScore(int score) {
-            this.score = score;
-        }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MultiplicationTableApp();
+            }
+        });
     }
 }
