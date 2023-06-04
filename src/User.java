03bc0ae784ec6509bdbc931d3c83791f7f4cdb06
Süleyman ÇETİN <suleyman.cetin.fc@outlook.com>
2023-06-05 00:26:15 +0300

    regular commit 2023-06-05

--- src/User.java
+++ src/User.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+    private int bestScore = 0;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public int getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(int bestScore) {
+        this.bestScore = bestScore;
+    }
 }
