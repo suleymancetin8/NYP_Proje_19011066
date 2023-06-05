899070293e0a4f7e22b1dd36c1567ed76400d249
Süleyman ÇETİN <suleyman.cetin.fc@outlook.com>
2023-06-05 03:14:07 +0300

    working one v2, it doesn't add the last questions point

--- src/MultiplicationLearningApp.java
+++ src/MultiplicationLearningApp.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,6 +11,8 @@ public class MultiplicationLearningApp {
     private int maxA = 5;
     private int maxB = 8;
     private int numQuestions = 3;
+    private int questionsAnswered = 0;
+    private int totalScore = 0;
     private Timer questionTimer;
     private int currentQuestionTime;
 
@@ -23,6 +24,7 @@ public class MultiplicationLearningApp {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
         showLoginScreen();
+        frame.setVisible(true);
     }
 
     private void showLoginScreen() {
@@ -42,16 +44,18 @@ public class MultiplicationLearningApp {
             for (User user : users) {
                 if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
                     currentUser = user;
+
                     if (user.isAdmin()) {
                         showParentScreen();
                     } else {
                         showChildScreen();
                     }
+
                     return;
                 }
             }
 
-            JOptionPane.showMessageDialog(frame, "Invalid credentials");
+            JOptionPane.showMessageDialog(frame, "Incorrect username or password");
         });
 
         panel.add(new JLabel("Username:"));
@@ -67,8 +71,9 @@ public class MultiplicationLearningApp {
 
     private void showParentScreen() {
         frame.getContentPane().removeAll();
+
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setLayout(new GridLayout(4, 2));
 
         JButton addChildButton = new JButton("Add Child");
         addChildButton.addActionListener(e -> showAddChildScreen());
@@ -76,14 +81,30 @@ public class MultiplicationLearningApp {
         JButton editChildButton = new JButton("Edit Child");
         editChildButton.addActionListener(e -> showEditChildScreen());
 
+        JTextField maxAField = new JTextField(String.valueOf(maxA));
+        JTextField maxBField = new JTextField(String.valueOf(maxB));
+        JTextField numQuestionsField = new JTextField(String.valueOf(numQuestions));
+
+        JButton saveSettingsButton = new JButton("Save Settings");
+        saveSettingsButton.addActionListener(e -> {
+            maxA = Integer.parseInt(maxAField.getText());
+            maxB = Integer.parseInt(maxBField.getText());
+            numQuestions = Integer.parseInt(numQuestionsField.getText());
+        });
+
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.addActionListener(e -> showLoginScreen());
+
         panel.add(addChildButton);
         panel.add(editChildButton);
-
-        for (User user : users) {
-            if (!user.isAdmin()) {
-                panel.add(new JLabel(user.getUsername() + ": " + user.getBestScore()));
-            }
-        }
+        panel.add(new JLabel("Max A:"));
+        panel.add(maxAField);
+        panel.add(new JLabel("Max B:"));
+        panel.add(maxBField);
+        panel.add(new JLabel("Number of Questions:"));
+        panel.add(numQuestionsField);
+        panel.add(saveSettingsButton);
+        panel.add(logoutButton);
 
         frame.getContentPane().add(panel);
         frame.revalidate();
@@ -105,19 +126,11 @@ public class MultiplicationLearningApp {
             String password = new String(passwordField.getPassword());
 
             if (username.isEmpty() || password.isEmpty()) {
-                JOptionPane.showMessageDialog(frame, "Please fill out all fields");
+                JOptionPane.showMessageDialog(frame, "Please enter a username and password");
                 return;
             }
 
-            for (User user : users) {
-                if (user.getUsername().equals(username)) {
-                    JOptionPane.showMessageDialog(frame, "Username already exists");
-                    return;
-                }
-            }
-
             users.add(new User(username, password, false));
-
             showParentScreen();
         });
 
@@ -136,60 +149,44 @@ public class MultiplicationLearningApp {
         frame.getContentPane().removeAll();
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JComboBox<User> userComboBox = new JComboBox<>();
         for (User user : users) {
             if (!user.isAdmin()) {
-                userComboBox.addItem(user);
+                JPanel userPanel = new JPanel();
+                userPanel.setLayout(new GridLayout(1, 4));
+
+                JLabel usernameLabel = new JLabel(user.getUsername());
+                JButton changePasswordButton = new JButton("Change Password");
+                JButton removeButton = new JButton("Remove");
+                JButton viewScoreButton = new JButton("View Score");
+
+                changePasswordButton.addActionListener(e -> {
+                    String newPassword = JOptionPane.showInputDialog(frame, "Enter a new password:");
+                    user.setPassword(newPassword);
+                });
+
+                removeButton.addActionListener(e -> {
+                    users.remove(user);
+                    showEditChildScreen();
+                });
+
+                viewScoreButton.addActionListener(e -> {
+                    JOptionPane.showMessageDialog(frame, "User's score: " + user.getBestScore());
+                });
+
+                userPanel.add(usernameLabel);
+                userPanel.add(changePasswordButton);
+                userPanel.add(removeButton);
+                userPanel.add(viewScoreButton);
+
+                panel.add(userPanel);
             }
         }
 
-        JTextField usernameField = new JTextField();
-        JPasswordField passwordField = new JPasswordField();
-
-        JButton saveButton = new JButton("Save");
-        saveButton.addActionListener(e -> {
-            User user = (User) userComboBox.getSelectedItem();
-
-            String username = usernameField.getText();
-            String password = new String(passwordField.getPassword());
-
-            if (username.isEmpty() || password.isEmpty()) {
-                JOptionPane.showMessageDialog(frame, "Please fill out all fields");
-                return;
-            }
-
-            for (User otherUser : users) {
-                if (otherUser != user && otherUser.getUsername().equals(username)) {
-                    JOptionPane.showMessageDialog(frame, "Username already exists");
-                    return;
-                }
-            }
-
-            user.setUsername(username);
-            user.setPassword(password);
-
-            showParentScreen();
-        });
-
-        JButton deleteButton = new JButton("Delete");
-        deleteButton.addActionListener(e -> {
-            User user = (User) userComboBox.getSelectedItem();
-
-            users.remove(user);
-
-            showParentScreen();
-        });
-
-        panel.add(new JLabel("User:"));
-        panel.add(userComboBox);
-        panel.add(new JLabel("Username:"));
-        panel.add(usernameField);
-        panel.add(new JLabel("Password:"));
-        panel.add(passwordField);
-        panel.add(saveButton);
-        panel.add(deleteButton);
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(e -> showParentScreen());
+        panel.add(backButton);
 
         frame.getContentPane().add(panel);
         frame.revalidate();
@@ -198,6 +195,17 @@ public class MultiplicationLearningApp {
 
     private void showChildScreen() {
         frame.getContentPane().removeAll();
+
+        if (questionsAnswered == numQuestions) {
+            if (totalScore > currentUser.getBestScore()) {
+                currentUser.setBestScore(totalScore);
+            }
+            questionsAnswered = 0;
+            totalScore = 0;
+            JOptionPane.showMessageDialog(frame, "You finished all the questions! Your total score is: " + currentUser.getBestScore());
+            showLoginScreen();
+            return;
+        }
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(2, 2));
@@ -209,18 +217,22 @@ public class MultiplicationLearningApp {
         JLabel questionLabel = new JLabel(a + " x " + b + " = ?");
         JTextField answerField = new JTextField();
         JLabel timerLabel = new JLabel();
+        JLabel scoreLabel = new JLabel("Current score: " + totalScore + ", Best score: " + currentUser.getBestScore());
+        JLabel questionCounterLabel = new JLabel("Question: " + (questionsAnswered + 1) + " / " + numQuestions);
 
         if (questionTimer != null && questionTimer.isRunning()) {
             questionTimer.stop();
         }
 
-        currentQuestionTime = 0;
+        currentQuestionTime = 60;
+        timerLabel.setText(currentQuestionTime + "s");
         questionTimer = new Timer(1000, e -> {
-            currentQuestionTime++;
+            currentQuestionTime--;
             timerLabel.setText(currentQuestionTime + "s");
 
-            if (currentQuestionTime == 60) {
+            if (currentQuestionTime == 0) {
                 JOptionPane.showMessageDialog(frame, "Time's up!");
+                questionsAnswered++;
                 showChildScreen();
             }
         });
@@ -228,27 +240,32 @@ public class MultiplicationLearningApp {
 
         JButton answerButton = new JButton("Answer");
         answerButton.addActionListener(e -> {
+            questionTimer.stop();
             int answer = Integer.parseInt(answerField.getText());
 
             if (answer == a * b) {
-                questionTimer.stop();
-                int score = 60 - currentQuestionTime;
-
-                if (score > currentUser.getBestScore()) {
-                    currentUser.setBestScore(score);
-                }
-
-                JOptionPane.showMessageDialog(frame, "Correct! Your score: " + score);
-                showChildScreen();
-            } else {
-                JOptionPane.showMessageDialog(frame, "Incorrect, try again.");
+                totalScore += currentQuestionTime;
             }
+
+            questionsAnswered++;
+            showChildScreen();
+        });
+
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.addActionListener(e -> {
+            questionTimer.stop();
+            questionsAnswered = 0;
+            totalScore = 0;
+            showLoginScreen();
         });
 
         panel.add(questionLabel);
         panel.add(answerField);
         panel.add(timerLabel);
+        panel.add(scoreLabel);
+        panel.add(questionCounterLabel);
         panel.add(answerButton);
+        panel.add(logoutButton);
 
         frame.getContentPane().add(panel);
         frame.revalidate();
@@ -256,6 +273,6 @@ public class MultiplicationLearningApp {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MultiplicationLearningApp::new);
+        new MultiplicationLearningApp();
     }
 }
